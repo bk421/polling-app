f7780a3bcab13f85c835481b1c4771d699430f77
bk421 <billyk77@gmail.com>
2018-07-26 14:11:08 -0400

    whatever

--- src/styledComponents/typography.js
+++ src/styledComponents/typography.js
@@ -4,3 +4,7 @@ import styled from 'styled-components';
 export const Heading2 = styled.h2`
   text-transform: capitalize;
 `;
+
+export const HeadingMenu = styled.h1`
+  text-transform: capitalize;
+`;
