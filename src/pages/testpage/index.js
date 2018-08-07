bdeb215831f8b4e74dea9e3ddfccf0599a46c465
bk421 <billyk77@gmail.com>
2018-08-07 15:52:04 -0400

    a

--- src/pages/testpage/index.js
+++ src/pages/testpage/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import Link from 'gatsby-link';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import MenuButton from '../../components/MenuButton/index';
-import NavMenuContainer from '../../components/NavMenuContainer/index';
+// import MenuButton from '../../components/MenuButton/index';
+// import NavMenuContainer from '../../components/NavMenuContainer/index';
 
 import '../../fonts/_fonts.css'
 
@@ -35,8 +35,8 @@ const testpage = () => (
     <p>Donec vestibulum faucibus augue, at egestas erat lacinia sed. Donec blandit, metus at iaculis vulputate, orci dui gravida est, et tempus urna neque eget turpis. Fusce ultrices ac leo a iaculis. Cras metus dui, ultrices non euismod at, lobortis at turpis. Praesent fringilla enim sit amet justo dignissim sagittis. Aliquam lobortis dolor at lorem hendrerit, sit amet blandit lectus consectetur. Aenean egestas nec odio sit amet posuere. Nam ac tortor id felis dapibus tincidunt eu vitae velit. Nam interdum elementum porttitor. Donec tempus posuere diam vitae malesuada. Cras pretium consectetur massa, sit amet sodales risus faucibus et. Aliquam tincidunt tempor risus, vel porttitor arcu consectetur in.</p>
     <p>Aenean leo ex, scelerisque quis efficitur egestas, auctor nec dolor. Praesent ullamcorper placerat tellus vestibulum dignissim. Mauris enim turpis, pharetra et aliquet lacinia, feugiat eu mi. Aenean porttitor pellentesque posuere. Morbi luctus efficitur est, ac iaculis tortor accumsan id. Suspendisse et ultricies lectus, et pharetra tortor. Cras turpis sem, mollis in porta in, porttitor sit amet elit.</p>
   </dummyPage>
-  <MenuButton />
-  <NavMenuContainer />
+  {/* <MenuButton />
+  <NavMenuContainer /> */}
 );
 
 export default testpage;
